1f19f307cd29df446a885cea55ba38cbfe465422
Jake Lang <phazon@protonmail.com>
2018-07-07 15:01:34 -0400

    skeleton for "main"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+/*
+ * eci-conform: Ethereum WebAssembly contract compliance tool
+ *
+ * Copyright (c) 2018 Jake Lang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#[macro_use] extern crate log;
+
+use std::env;
+use std::process;
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+
+fn print_help_and_exit() -> ! {
+    println!("Usage: eci-conform input.wast");
+    process::exit(-1);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli_args: Vec<String> = env::args().collect();
+
+    if cli_args.len() < 2 {
+        print_help_and_exit();
+    }
+    
+    let input_path = Path::new(&cli_args[1]);
+
+    debug!("Attempting to open file {}", input_path.display());
+    assert!(input_path.exists());
+
+    let mut fp = File::open(input_path).unwrap();
+    let mut wast_input = String::new();
+
+    fp.read_to_string(&mut wast_input);
 }
